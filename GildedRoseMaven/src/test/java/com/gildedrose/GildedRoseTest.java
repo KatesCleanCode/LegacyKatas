f77c609339f4026ef380dc19b7661d3990803099
Katharina Knaus <kknaus@it-economics.de>
2016-07-09 19:10:42 +0200

    refactor requirements presentation

--- GildedRoseMaven/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRoseMaven/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -15,24 +15,6 @@ import static org.hamcrest.Matchers.equalTo;
 
 import static org.junit.Assert.assertThat;
 
-/**
- * fast - The Quality of an item is never negative - "Aged Brie"
- * actually increases in Quality the older it gets - The Quality of
- * an item is never more than 50 - "Sulfuras", being a legendary
- * item, never has to be sold or decreases in Quality -
- * "Backstage passes", like aged brie, increases in Quality as its
- * SellIn value approaches; Quality increases by 2 when there are
- * 10 days or less and by 3 when there are 5 days or less but
- * Quality drops to 0 after the concert
- *
- * We have recently signed a supplier of conjured items. This
- * requires an update to our system:
- *
- * - "Conjured" items degrade in Quality twice as fast as normal
- * items
- *
- *
- */
 @RunWith(JUnitPlatform.class)
 public class GildedRoseTest {
 
@@ -44,6 +26,22 @@ public class GildedRoseTest {
   item = ItemTestDataFactory.getStandardItem();
   gildedRose = new GildedRose(new Item[] { item });
  }
+
+// - Once the sell by date has passed, Quality degrades twice as
+// fast
+// - The Quality of an item is never negative
+// - "Aged Brie" actually increases in Quality the older it gets
+// - The Quality of an item is never more than 50
+// - "Sulfuras", being a legendary item, never has to be sold or
+// decreases in Quality
+// - "Backstage passes", like aged brie, increases in Quality as
+// its SellIn value approaches;
+// Quality increases by 2 when there are 10 days or less and by 3
+// when there are 5 days or less but
+// Quality drops to 0 after the concert
+//
+// - "Conjured" items degrade in Quality twice as fast as normal
+// items
 
  @Test
  void qualityOfAnItemIsNeverNegative() {
