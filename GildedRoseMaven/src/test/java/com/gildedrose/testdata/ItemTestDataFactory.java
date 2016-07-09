b7ef2e160ab73249a40b0f8b8946f0228f8ff4b7
Katharina Knaus <kknaus@it-economics.de>
2016-07-09 19:41:23 +0200

    refactor itemtestdatafactory

--- GildedRoseMaven/src/test/java/com/gildedrose/testdata/ItemTestDataFactory.java
+++ GildedRoseMaven/src/test/java/com/gildedrose/testdata/ItemTestDataFactory.java
@@ -6,12 +6,12 @@ import com.gildedrose.UpdatableItem;
 public class ItemTestDataFactory {
  public static final int STANDARD_SELL_IN = 10;
  public static final int STANDARD_QUALITY = 20;
+ public static final int MINIMUM_QUALITY = 0;
+ public static final int MAX_QUALITY = 50;
 
  public static Item getStandardItem() {
   return new Item("standard", STANDARD_SELL_IN, STANDARD_QUALITY);
  }
-
- public static final int MINIMUM_QUALITY = 0;
 
  public static Item getAgedBrie() {
   return new Item("Aged Brie", STANDARD_SELL_IN, STANDARD_QUALITY);
@@ -20,8 +20,6 @@ public class ItemTestDataFactory {
  public static UpdatableItem getUpdatableAgedBrie() {
   return new UpdatableItem(getAgedBrie());
  }
-
- public static final int MAX_QUALITY = 50;
 
  public static UpdatableItem getUpdatableStandardItem() {
   return new UpdatableItem(getStandardItem());
