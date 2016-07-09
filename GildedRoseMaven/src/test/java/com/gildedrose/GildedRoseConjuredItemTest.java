3339a340fcb201bd5c926e7361436013f3a8bca4
Katharina Knaus <kknaus@it-economics.de>
2016-07-09 20:16:29 +0200

    add itemsDegradeInQualityByTwo

--- GildedRoseMaven/src/test/java/com/gildedrose/GildedRoseConjuredItemTest.java
+++ GildedRoseMaven/src/test/java/com/gildedrose/GildedRoseConjuredItemTest.java
@@ -1,6 +1,10 @@
 package com.gildedrose;
 
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 import com.gildedrose.testdata.ItemTestDataFactory;
 
@@ -9,13 +13,19 @@ public class GildedRoseConjuredItemTest {
  private GildedRose gildedRose;
  private UpdatableItem item;
 
-// - "Conjured" items degrade in Quality twice as fast as normal
-// items
-
  @BeforeEach
  void setUp() {
   item = ItemTestDataFactory.getUpdatableConjuredItem();
   gildedRose = new GildedRose(item);
+ }
+
+ @Test
+ @Disabled
+ void itemsDegradeInQualityByTwo() {
+  gildedRose.updateQuality();
+// TODO not implemented yet
+  Assert.assertThat(item.getQuality(),
+   Matchers.equalTo(ItemTestDataFactory.STANDARD_QUALITY - 2));
  }
 
 }
