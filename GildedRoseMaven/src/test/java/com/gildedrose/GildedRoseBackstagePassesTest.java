0a202e09daa494382f59567501c82ebaf85311a7
Katharina Knaus <kknaus@it-economics.de>
2016-07-09 19:59:06 +0200

    add backstagePassesIncreasesInQualityAsOlderItGets and change standard sell in

--- GildedRoseMaven/src/test/java/com/gildedrose/GildedRoseBackstagePassesTest.java
+++ GildedRoseMaven/src/test/java/com/gildedrose/GildedRoseBackstagePassesTest.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import com.gildedrose.testdata.ItemTestDataFactory;
 
@@ -12,6 +15,20 @@ public class GildedRoseBackstagePassesTest {
  void setUp() {
   item = ItemTestDataFactory.getUpdatableBackstagePasses();
   gildedRose = new GildedRose(item);
+ }
+
+// - "Backstage passes", like aged brie, increases in Quality as
+// its SellIn value approaches;
+// Quality increases by 2 when there are 10 days or less and by 3
+// when there are 5 days or less but
+// Quality drops to 0 after the concert
+//
+ @Test
+ void backstagePassesIncreasesInQualityAsOlderItGets() {
+  gildedRose.updateQuality();
+
+  Assert.assertThat(item.getQuality(),
+   Matchers.equalTo(ItemTestDataFactory.STANDARD_QUALITY + 1));
  }
 
 }
